b9311804b998e75e64e268e3e78f375ea83338a5
Kristi <kristiqato1@gmail.com>
2018-05-19 17:48:52 +0200

    Add Map and SBM Vertex partition algorithms

---
+++ src/MapVertexPartition.cpp
@@ -0,0 +1,191 @@
+//  MapEquationVertexPartition.cpp
+//  kristo
+//
+//  Created by Kristi Qato on 21/06/18.
+//
+
+#include "MapVertexPartition.h"
+#include <math.h>
+#include <iostream>
+
+
+MapVertexPartition::MapVertexPartition(Graph* graph,
+                                       vector<size_t> const& membership) :
+MutableVertexPartition(graph,
+                       membership)
+{ }
+
+MapVertexPartition::MapVertexPartition(Graph* graph) :
+MutableVertexPartition(graph)
+{ }
+
+MapVertexPartition* MapVertexPartition::create(Graph* graph)
+{
+    return new MapVertexPartition(graph);
+}
+
+MapVertexPartition*  MapVertexPartition::create(Graph* graph, vector<size_t> const& membership)
+{
+    return new  MapVertexPartition(graph, membership);
+}
+
+MapVertexPartition::~MapVertexPartition()
+{ }
+
+//plogp
+double MapVertexPartition::plogp(double p)
+{
+    return p > 0.0 ? p*log(p) : 0;
+}
+
+double MapVertexPartition::diff_move(size_t v, size_t new_comm)
+{
+    //std::cout << "double MapEquationVertexPartition::diff_move(" << v << ", " << new_comm << ")" << endl;
+
+    double diff = 0.0;
+    size_t old_comm = this->membership(v);
+    double t_w = 2*this->graph->total_weight();
+    double w = 2*this->total_weight_in_all_comms();
+    //std::cout << "Total weight " << t_w << endl;
+    if (new_comm != old_comm){
+        
+       
+        
+        //L(M) = w_exit*log(w_exit)-2sum(m,i=1)w_m_exit*log(w_m_exit)+sum(m,i=1)(w_m_exit+w_m)*log(w_m_exit+w_m)
+        
+        double v_s = this->graph->strength(v, IGRAPH_ALL);
+        //old comm
+        double in_old = this->total_weight_in_comm(old_comm); // internal weight of the old_comm
+        double w_v_old = this->weight_from_comm(v,old_comm);// the total weight between v and old_com
+        double w_old = this->total_weight_from_comm(old_comm);// the total weight of links in old_comm
+        //new comm
+        double in_new = this->total_weight_in_comm(new_comm);// internal weight of the new_comm
+        double w_v_new = this->weight_from_comm(v,new_comm);// the total weight between v and new_Comm
+        double w_new = this->total_weight_from_comm(new_comm);//the total weight of links in new_com
+        
+        //std::cout << "Comm\tInternal weight\tWeight between\tTotal weight" << std::endl;
+        //std::cout << old_comm << "\t" << in_old << "\t" << w_v_old << "\t" << w_old << std::endl;
+        //std::cout << new_comm << "\t" << in_new << "\t" << w_v_new << "\t" << w_new << std::endl;
+        //Before move v to the other community L(M)
+        
+        // the total weight of the link exiting old module
+        double exit_old = w_old - 2*in_old; // the links that goes outside the old module
+        //std::cout << "Before move: Old module exit weight is " << exit_old << std::endl;
+        // the total weight of the links exiting new module
+        double exit_new = w_new - 2*in_new;
+        //std::cout << "Before move: new module exit weight is " << exit_new << std::endl;
+        
+        // the total weight between modules
+        double w_m = t_w-w; // the weights between modules
+        double w_b_m = w_m /t_w; //normalized
+        //std::cout << "Before move: Total weight between is " << w_m << std::endl;
+        //std::cout << "Before move: Total normalised weight between is " << w_b_m << std::endl;
+        
+        //After the move v to the new community
+        
+        //old module
+        double w_old1 = w_old - v_s; // the weight of old module
+        //std::cout << "After move: Old module weight is " << w_old1 << std::endl;
+        double exit_v = v_s - w_v_old;   // the total weight of the links that link v with other nodes but not the nodes inside the old_comm
+        //std::cout << "After move: Exit v old " << exit_v << std::endl;
+        double exit_old1 = exit_old - exit_v + w_v_old; // the total exiting weights of old_comm
+        //std::cout << "After move: Old module exit weight is " << exit_old1  << std::endl;
+        
+        //new module
+        double w_new1 = w_new + v_s; //the total weight of the new module
+        //std::cout << "After move: New module weight is " << w_new1 << std::endl;
+        double exit_v_new = v_s - w_v_new; // all the links that go outside v to new_comm but not with the nodes in the new_comm
+        //std::cout << "After move: Exit v new " << exit_v_new << std::endl;
+        double exit_new1 = exit_new + exit_v_new - w_v_new;// the total exiting weight of new comm
+        //std::cout << "After move: new module exit weight is " << exit_new1  << std::endl;
+        
+        // the total weight between modules
+        double w_m1 = w_m + 2*w_v_old - 2*w_v_new;
+        double w_b_m1 = w_m1/t_w;
+        //std::cout << "After move: Total weight between is " << w_m1 << std::endl;
+        //std::cout << "After move: Total normalised weight between is " << w_b_m1 << std::endl;
+        
+        // codelength term  (delta between terms)
+        
+        // the exit weight
+        double old_module_exit = -2*plogp(exit_old1/t_w) + 2*plogp(exit_old/t_w);
+        //std::cout << "Old module exit weight is " << old_module_exit << endl;
+        
+        double new_module_exit = -2*plogp(exit_new1/t_w) + 2*plogp(exit_new/t_w);
+        //std::cout << "New module exit weight is " << new_module_exit << endl;
+        
+        double module_exit_diff = new_module_exit + old_module_exit;
+        //std::cout << "The exit weights are " << module_exit_diff << endl;
+        
+        // the exit weight between modules
+        double exit_between_modules_diff = plogp(w_b_m1) - plogp(w_b_m);
+        //std::cout << "The weight between modules is: " << exit_between_modules_diff << endl;
+        
+        // the module weight
+        double old_module_weight = plogp((exit_old1 + w_old1)/t_w) - plogp((exit_old + w_old)/t_w);
+        //std::cout << "Old module weight difference is " << old_module_weight << endl;
+        double new_module_weight = plogp((exit_new1 + w_new1)/t_w) - plogp((exit_new + w_new)/t_w);
+        //std::cout << "New module weight difference is " << new_module_weight << endl;
+        double module_weight_diff = new_module_weight + old_module_weight;
+        //std::cout << "Module weight diffference is " << module_weight_diff << endl;
+        
+        // Calculation of L(M)
+        
+        diff = exit_between_modules_diff + module_exit_diff + module_weight_diff;
+        //std::cout << "The diffference is " << diff << endl;
+    }
+    return -diff;
+}
+
+
+double MapVertexPartition::quality()
+{
+#ifdef DEBUG
+    cerr << "double MapEquationVertexPartition::quality()" << endl;
+#endif
+    double mod= 0.0;
+    double total_weight = 2*this->graph->total_weight();
+    double w = 2*this->total_weight_in_all_comms();
+#ifdef DEBUG
+    cerr << "\t" << "total_weight=" << total_weight << "." << endl;
+#endif
+    double all = 1 - w/total_weight;
+    mod = plogp(all);
+    
+    for (size_t c = 0; c < this->nb_communities(); c++)
+    {
+        
+        
+        // the total relative links of the community in the network;
+        double comm_weight = this->total_weight_from_comm(c);
+        // the internal weight of the community
+        double module_weight = this->total_weight_in_comm(c);
+        // the exit links
+        double exit_weight= comm_weight-2*module_weight;
+        
+        //all the links between modules
+        //double all = (total_weight -w)/total_weight;
+#ifdef DEBUG
+        cerr << "\t" << ", comm_weight=" << comm_weight << ", module_weight=" << module_weight << ", exit_weights" << exit_weights << "all" << all << "." << endl;
+#endif
+        //cacluate the codelengths
+        
+        mod += -2*plogp(exit_weight/total_weight) + plogp((exit_weight + comm_weight)/total_weight);
+    }
+    
+    for (size_t v = 0; v < this->graph->vcount(); v++)
+    {
+        double v_s = this->graph->strength(v, IGRAPH_ALL);
+        v_s /= total_weight;
+        mod += plogp(v_s);
+    }
+    
+    
+#ifdef DEBUG
+    cerr << "exit MapEquationVertexPartition::quality()" << endl;
+    cerr << "return " << mod << endl << endl;
+#endif
+    return -mod;
+}
+
+
---
+++ src/SBMVertexPartition.cpp
@@ -0,0 +1,199 @@
+
+//  SBMVertexPartition.cpp
+//  kristo
+//
+//  Created by Kristi Qato on 21/06/18.
+//
+
+#include "SBMVertexPartition.h"
+
+#include <iostream>
+#include <math.h>
+
+
+
+SBMVertexPartition::SBMVertexPartition(Graph* graph,
+                                       vector<size_t> const& membership) :
+MutableVertexPartition(graph,
+                       membership)
+{ }
+
+SBMVertexPartition::SBMVertexPartition(Graph* graph) :
+MutableVertexPartition(graph)
+{ }
+
+SBMVertexPartition* SBMVertexPartition::create(Graph* graph)
+{
+    return new SBMVertexPartition(graph);
+}
+
+SBMVertexPartition*  SBMVertexPartition::create(Graph* graph, vector<size_t> const& membership)
+{
+    return new  SBMVertexPartition(graph, membership);
+}
+
+SBMVertexPartition::~SBMVertexPartition()
+{ }
+
+
+// calculatoin of  a and b
+double SBMVertexPartition:: plogp(double p)
+{
+    return p > 0.0 ? p*log(p) : 0.0;
+}
+
+//double SBMVertexPartition:: clogc (double c)
+//{
+//    return c > 0.0 ? c * log2(c) : 0.0;
+//}
+double SBMVertexPartition:: getWeightBetweenCommunities(size_t r, size_t s)
+{
+    double weight_in_between = 0.0;
+    set<size_t> comm_nodes = this->get_community(r);
+    for (std::set<size_t>::iterator it = comm_nodes.begin();
+         it != comm_nodes.end(); ++it)
+    {
+        size_t u = *it; // u is a node in r
+        weight_in_between += this->weight_to_comm(u, s);
+    }
+    if (s == r)
+        weight_in_between /= 2.0;
+    return weight_in_between;
+}
+
+// diff function
+
+double SBMVertexPartition::diff_move(size_t v, size_t new_comm)
+{
+    
+    //cerr << "double MapEquationVertexPartition::diff_move(" << v << ", " << new_comm << ")" << endl;
+    
+    std::cout << "\n \n double SBMEquationVertexPartition::diff_move(" << v << ", " << new_comm << ")" << endl;
+    
+    double diff = 0.0;
+    size_t old_comm = this->membership(v);
+    
+    
+    
+    if (new_comm != old_comm)
+    {
+        for (size_t t = 0; t < this->nb_communities(); t++)
+        {
+            
+            if (t != old_comm && t != new_comm)
+            {
+                double m_rs = this->getWeightBetweenCommunities(old_comm,new_comm);
+                std::cout<<"M_rs: "<<m_rs<<endl;
+                double m_rt = this-> getWeightBetweenCommunities(old_comm, t);
+                double m_st = this-> getWeightBetweenCommunities(new_comm, t);
+                std::cout <<"M_rt: "<< m_rt<<endl;
+                std::cout <<"M_st: "<<m_st<<endl;
+                double k_it = this->getWeightBetweenCommunities(v,t);
+                std::cout <<"K_it: " << k_it<<endl;
+                double k_ir = this->weight_to_comm(v, old_comm);
+                std::cout<<"K_ir : "<< k_ir<< endl;
+                double k_is = this->weight_to_comm(v, new_comm);
+                std::cout<<"K_is : "<< k_is<< endl;
+                double m_rr = this->total_weight_in_comm(old_comm);
+                double m_ss = this->total_weight_in_comm(new_comm);
+                std::cout <<"M_rr: "<< m_rr<<endl;
+                std::cout <<"M_ss: "<<m_ss<<endl;
+                
+                
+                double k_i = this->graph->strength(v, IGRAPH_ALL);
+                //double u_i = this->graph->node_self_weight(v);
+                std::cout <<"K_i: "<< k_i<<endl;
+                
+                
+                double kappa_r = this->total_weight_from_comm(old_comm);
+                std::cout<<"Kappa r : "<< kappa_r<< endl;
+                double kappa_s = this->total_weight_from_comm(new_comm);
+                std::cout<<"Kappa s: "<< kappa_s << endl;
+                
+                
+              
+                //old community after
+                double m_rt1 = m_rt-2*k_it;
+                double m_st1 = m_st+2*k_it;
+                std::cout <<"M_rt1 : "<< m_rt1 <<endl;
+                std::cout <<"M_st1 : "<< m_st1 <<endl;
+               
+                
+                //calculate the difference
+               
+                double between_comm_t  = 2*plogp(m_rt1)- 2*plogp(m_rt) + 2*plogp(m_st1)-2*plogp(m_st);
+                
+               
+                
+                double m_rs1 = m_rs +k_ir-k_is;
+                
+                double M_rs = 2*plogp(m_rs1)-2*plogp(m_rs);
+                std::cout <<"M_rs1 : "<< m_rs1 <<endl;
+               
+                
+                
+                double m_rr1 = m_rr-k_ir;
+                double m_ss1 = m_ss+k_is;
+               
+                double in_comm_weights = plogp(m_rr1) - plogp(m_rr) +plogp(m_ss1)- plogp(m_ss);
+                std::cout <<"m_rr1 : "<< m_rr1 <<endl;
+                std::cout <<"m_ss1 : "<< m_ss1 <<endl;
+               
+                
+                double kappa_r1 = kappa_r-k_i;
+                double kappa_s1 = kappa_s+k_i;
+                
+                double diff_kappa = -2*plogp(kappa_r1)+2*plogp(kappa_r) -2*plogp(kappa_s1) +2*plogp(kappa_s);
+                std::cout <<"kapp_r1 : "<< kappa_r1 <<endl;
+                std::cout <<"kapp_s1 : "<< kappa_s1 <<endl;
+                
+            
+                std::cout <<"between_comm_t : "<< between_comm_t <<endl;
+                std::cout <<"M_rs : "<< M_rs <<endl;
+                std::cout <<"in_comm_weights:  "<< in_comm_weights<<endl;
+                std::cout <<"diff_kappa : "<< diff_kappa <<endl;
+                diff += between_comm_t +M_rs + in_comm_weights + diff_kappa;
+                
+                
+                std::cout<<"DIFF : "<< diff<< endl;
+            
+                
+               
+            }
+           
+            
+        }
+       
+       return diff;
+        
+    }
+    //return diff;
+}
+// the quality
+double SBMVertexPartition::quality()
+{
+    //#ifdef DEBUG
+    //cerr << "double SBMEquationVertexPartition::quality()" << endl;
+    //#endif
+    double sbm =0.0;
+    
+    
+    for (size_t r = 0; r < this->nb_communities(); r++)
+    {
+        for (size_t s = 0; s < this->nb_communities(); s++)
+        {
+            double weight_between = this->getWeightBetweenCommunities(r, s);
+            double total_weight_r = this->total_weight_from_comm(r);
+            double total_weight_s = this->total_weight_to_comm(s);
+            cerr << "In between " << r << " and " << s << endl;
+            cerr << "weight_between = " << weight_between << endl;
+            cerr << "k_r = " << total_weight_r << endl;
+            cerr << "k_s = " << total_weight_s << endl;
+            if (weight_between > 0)
+                sbm += weight_between*log(weight_between/(total_weight_r*total_weight_s));
+        }
+    }
+    
+    return sbm;
+}
+
